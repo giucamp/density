ea69a9844b16fb29c7520f156b1c4360dda43f84
giucamp <giu.campana@gmail.com>
2017-09-28 13:47:21 +0200

    Update test.bat

--- test/vs2017/test.bat
+++ test/vs2017/test.bat
@@ -2,6 +2,10 @@ REM SET CONFIGURATION=Release
 REM SET PLATFORM=x64
 
 IF "%CONFIGURATION%"=="Release" (
-    ECHO Testing "%PLATFORM% %CONFIGURATION%"
-    %PLATFORM%\%CONFIGURATION%\density_tests.exe
+    ECHO Testing %PLATFORM% %CONFIGURATION%
+    IF "%PLATFORM%"=="x64" (
+        test\vs2017\x64\%CONFIGURATION%\density_tests.exe
+    ) ELSE (
+        test\vs2017\Win32\%CONFIGURATION%\density_tests.exe
+    )    
 )
