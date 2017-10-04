6b70e331d475d9e0395b7943141ac29d6030b289
giuseppe.campana <giu.campana@gmail.com>
2017-10-04 07:43:12 +0200

    another lifo_array benchmark

--- bench/tests/lifo_tests.cpp
+++ bench/tests/lifo_tests.cpp
@@ -44,9 +44,35 @@ namespace density_bench
         i_tree["lifo_tests_1"].add_performance_test(group);
     }
 
+    void lifo_tests_2(TestTree & i_tree)
+    {
+        PerformanceTestGroup group("bench", "");
+       
+        using namespace density;
+
+        group.set_cardinality_start(16);
+        group.set_cardinality_end(4000);
+        group.set_cardinality_step(1);
+
+        group.add_test(__FILE__, __LINE__, [](size_t i_cardinality) {
+            lifo_array<double> chars(i_cardinality);
+            volatile double c = 0;
+            chars[0] = c;
+        }, __LINE__);
+
+        group.add_test(__FILE__, __LINE__, [](size_t i_cardinality) {
+            auto chars = static_cast<double*>(_alloca(i_cardinality * sizeof(double)));
+            volatile double c = 0;
+            chars[0] = c;
+        }, __LINE__);
+
+        i_tree["lifo_tests_2"].add_performance_test(group);
+    }
+
     void lifo_tests(TestTree & i_tree)
     {
         lifo_tests_1(i_tree);
+        lifo_tests_2(i_tree);
     }
 }
 
