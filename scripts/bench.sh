0f0d0f8124498b662f63bbf7186d8505619ed418
giuseppe.campana <giu.campana@gmail.com>
2017-10-07 11:58:53 +0200

    travis: passing variables to scrpits as parameters

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+RUN=$1
+
 cd bench
 mkdir build
 cd build
 cmake ..
-echo "$BENCH_RUN = $BENCH_RUN"
+echo "RUN = $RUN"
 make
-if [ "$BENCH_RUN" = "TRUE" ]; then
+if [ "$RUN" = "TRUE" ]; then
     sudo make install
     density_bench
 fi
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+RUN=$1
+
 cd test
 mkdir build
 cd build
 cmake ..
-echo "TEST_RUN = $TEST_RUN" 
+echo "RUN = $RUN"
 make
-if [ "$TEST_RUN" = "TRUE" ]; then
+if [ "$RUN" = "TRUE" ]; then
     sudo make install
     density_test
 fi
