6726ddb731a812492848bb962aa889458d7648db
giuseppe.campana <giu.campana@gmail.com>
2017-10-07 11:40:03 +0200

    travis: env variables fixes

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -4,6 +4,7 @@ cd bench
 mkdir build
 cd build
 cmake ..
+echo "$BENCH_RUN = $BENCH_RUN"
 make
 if [ "$BENCH_RUN" = "TRUE" ]; then
     sudo make install
--- scripts/test.sh
+++ scripts/test.sh
@@ -4,6 +4,7 @@ cd test
 mkdir build
 cd build
 cmake ..
+echo "TEST_RUN = $TEST_RUN" 
 make
 if [ "$TEST_RUN" = "TRUE" ]; then
     sudo make install
