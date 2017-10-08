e857298e3373f3bf90c9c51aaacf84705ca45bb1
giuseppe.campana <giu.campana@gmail.com>
2017-10-08 16:54:47 +0200

    enabling coverage in gcc

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 RUN=$1
+DEBUG=&2
+COVERAGE=&3
+
+echo "RUN = $RUN, DEBUG = $DEBUG, COVERAGE = $COVERAGE"
+
+if [ "$DEBUG" = "TRUE" ]; then
+    MAKE_ARGS+=" -DCMAKE_BUILD_TYPE=Debug"
+fi
+
+if [ "$COVERAGE" = "TRUE" ]; then
+    GCC_OPTIONS+=" -fprofile-arcs -ftest-coverage"
+fi
 
 cd test
 mkdir build
 cd build
-cmake ..
-echo "RUN = $RUN"
+cmake .. "$MAKE_ARGS" -DCOMPILER_EXTRA:STRING=MAKE_ARGS
 make
 if [ "$RUN" = "TRUE" ]; then
     sudo make install
