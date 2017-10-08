b14f2963b1fb605997941a49dfb9fc7e13580cda
giuseppe.campana <giu.campana@gmail.com>
2017-10-08 12:37:10 +0200

    update publish script [skip ci]

--- scripts/merge_to_master_and_gen_doc.bat
+++ scripts/merge_to_master_and_gen_doc.bat
@@ -4,14 +4,17 @@ ECHO This script updates the doxygen doc and merges 'develop' to 'master'
 ECHO ***********************
 ECHO ON
 PAUSE
-PAUSE
-PAUSE
 
 SET git=C:\Users\giuca\AppData\Local\Atlassian\SourceTree\git_local\bin\git.exe
 %git% checkout master
+PAUSE
 %git% merge develop --commit --verbose
+PAUSE
 CALL generate_doc.bat
+PAUSE
 %git% add C:\projects\density\doc\html\* --force
+PAUSE
 %git% commit -m"Merging develop to master"
+PAUSE
 %git% push
 PAUSE
