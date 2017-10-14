2d8a4aa0409428863e1871c372927ae32b3a9931
giucamp <giu.campana@gmai.com>
2017-10-14 15:52:41 +0200

    cmake: added ENABLE_TEST_DATA_STACK, gcov fix

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,12 +2,17 @@
 
 RUN=$1
 DEBUG=$2
-GCOV=$3
+TEST_DATA_STACK=$3
+GCOV=$4
 
 echo "RUN = $RUN, DEBUG = $DEBUG, GCOV = $GCOV"
 
 if [ "$DEBUG" = "TRUE" ]; then
     MAKE_ARGS+=" -DCMAKE_BUILD_TYPE=Debug"
+fi
+
+if [ "$TEST_DATA_STACK" = "TRUE" ]; then
+    MAKE_ARGS+=" -DENABLE_TEST_DATA_STACK=ON"
 fi
 
 if [ "$GCOV" != "" ]; then
@@ -25,7 +30,7 @@ if [ "$RUN" = "TRUE" ]; then
 fi
 
 cd ../..
-if [ "$COVERAGE" = "TRUE" ]; then
+if [ "$GCOV" != "" ]; then
     $GCOV -version
     coveralls --verbose --gcov '$GCOV' --gcov-options '\-lp' -b test/build/CMakeFiles/density_test.dir
 fi
