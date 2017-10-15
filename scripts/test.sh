af7fc0c35fd6f73a3fe5d21c8cf25206400059d7
giucamp <giu.campana@gmai.com>
2017-10-15 15:08:59 +0200

    fixed DENSITY_USER_DATA_STACK not defined

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,29 +4,32 @@ RUN=$1
 DEBUG=$2
 TEST_DATA_STACK=$3
 GCOV=$4
+PARAMS=$5
 
-echo "RUN = $RUN, DEBUG = $DEBUG, GCOV = $GCOV"
+echo "RUN=$RUN, DEBUG=$DEBUG, TEST_DATA_STACK=$TEST_DATA_STACK, GCOV=$GCOV, PARAMS=$PARAMS"
 
 if [ "$DEBUG" = "TRUE" ]; then
     MAKE_ARGS+=" -DCMAKE_BUILD_TYPE=Debug"
 fi
 
 if [ "$TEST_DATA_STACK" = "TRUE" ]; then
-    MAKE_ARGS+=" -DENABLE_TEST_DATA_STACK=ON"
+    MAKE_ARGS+=" -DENABLE_TEST_DATA_STACK:BOOL=ON"
 fi
 
 if [ "$GCOV" != "" ]; then
     GCC_OPTIONS+=" -fprofile-arcs -ftest-coverage"
 fi
 
+echo "MAKE_ARGS = $MAKE_ARGS"
+
 cd test
 mkdir build
 cd build
-cmake "$MAKE_ARGS" -DCOMPILER_EXTRA:STRING="$GCC_OPTIONS" ..
+cmake $MAKE_ARGS -DCOMPILER_EXTRA:STRING="$GCC_OPTIONS" ..
 make
 if [ "$RUN" = "TRUE" ]; then
     sudo make install
-    density_test
+    density_test $PARAMS
 fi
 
 cd ../..
